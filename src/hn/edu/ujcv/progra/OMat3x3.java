585b65d128e014d61d8517388e8dac62af0449c6
Kiana <kianapelgrim@outlook.com>
2020-03-10 22:18:59 -0600

    Transpuesta y constructores de mat3x3

--- src/hn/edu/ujcv/progra/OMat3x3.java
+++ src/hn/edu/ujcv/progra/OMat3x3.java
@@ -6,6 +6,8 @@ public class OMat3x3 {
     private OVecR3 col1;
     private OVecR3 col2;
     private OVecR3 col3;
+
+
     // accesoras y mutadoras
     public OVecR3 getColumna1(){
         return col1;
@@ -58,7 +60,9 @@ public class OMat3x3 {
 
     public OMat3x3 transpuesta(){
         //TODO: implementar
-        return new OMat3x3();
+        return new OMat3x3(col1.getA(), col2.getA(), col3.getA(),
+                col1.getB(), col2.getB(), col3.getB2(), 
+                col1.getB2(), col2.getB2(), col3.getB2());
     }
 
     public OMat3x3 inversa(){
@@ -119,6 +123,19 @@ public class OMat3x3 {
     }
 
     // constructores
+    public OMat3x3(){}
+
+    public OMat3x3(OVecR3 a){}
+
+    public OMat3x3(double m11, double m12, double m13,
+                   double m21, double m22, double m23,
+                   double m31, double m32, double m33){}
+
+
+
+    public OMat3x3(OVecR2 a, OVecR2 b, boolean esColumna,OVecR3 col3){}
+    public OMat3x3( OVecR3 col1, OVecR3 col2,OVecR3 col3) {
+    }
 
 
 }
